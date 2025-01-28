38e95609bfadfd7e55deffe4b25a1041d9c35fc7
Radhika Jayaprakash <85903942+rjayapra@users.noreply.github.com>
2025-01-28 15:29:11 -0500

    initial commit

---
+++ src/main/java/com/function/Function.java
@@ -0,0 +1,45 @@
+package com.function;
+
+import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.annotation.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Optional;
+
+/**
+ * Azure Functions with HTTP Trigger.
+ */
+public class Function {
+    private static final Logger logger = LogManager.getLogger(Function.class);
+
+    /**
+     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
+     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
+     */
+    @FunctionName("HttpExample")
+    public HttpResponseMessage run(
+            @HttpTrigger(
+                name = "req",
+                methods = {HttpMethod.GET, HttpMethod.POST},
+                authLevel = AuthorizationLevel.ANONYMOUS)
+                HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+        logger.info("Log4j : Java HTTP trigger processed a request.");
+
+        // Parse query parameter
+        final String query = request.getQueryParameters().get("name");
+        final String name = request.getBody().orElse(query);
+
+        if (name == null) {
+            logger.warn("Name parameter is missing in the request.");
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+        } else {
+            logger.info("Name parameter found: {}", name);
+            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+        }
+        
+    }
+}
