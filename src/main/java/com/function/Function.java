c9c45730f3e9ff2d73e3631d79f192f8370094cc
Radhika Jayaprakash <85903942+rjayapra@users.noreply.github.com>
2025-02-21 17:14:49 -0500

    updates for windows asp

--- src/main/java/com/function/Function.java
+++ src/main/java/com/function/Function.java
@@ -2,8 +2,11 @@ package com.function;
 
 import com.microsoft.azure.functions.*;
 import com.microsoft.azure.functions.annotation.*;
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Level;
+
 
 import java.util.Optional;
 
@@ -12,6 +15,7 @@ import java.util.Optional;
  */
 public class Function {
     private static final Logger logger = LogManager.getLogger(Function.class);
+    private static final Level AUDIT = Level.getLevel("AUDIT");
 
     /**
      * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
@@ -27,6 +31,11 @@ public class Function {
                 HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
+        String invocationId = context.getInvocationId();
+
+        // Setting invocation id to Log4j context for correlation of execution context logs and log4j logs in AI
+        ThreadContext.put("InvocationId", invocationId);
+        ThreadContext.put("FunctionName", context.getFunctionName());
         logger.info("Log4j : Java HTTP trigger processed a request.");
 
         // Parse query parameter
@@ -35,9 +44,11 @@ public class Function {
 
         if (name == null) {
             logger.warn("Name parameter is missing in the request.");
+            logger.log(AUDIT, "Audit log event from Log4j. Name: " + name + ", InvocationID: " + invocationId);  
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
         } else {
             logger.info("Name parameter found: {}", name);
+            logger.log(AUDIT, "Audit log event from Log4j. Name: " + name + ", InvocationID: " + invocationId);  
             return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
         }
         
